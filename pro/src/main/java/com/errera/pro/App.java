a1553bb899064eff3e84f6cf22e8d7405ee86e8a
anthony <anthonyehds>
2021-05-24 16:52:02 +0200

    initial commit

---
+++ pro/src/main/java/com/errera/pro/App.java
@@ -0,0 +1,12 @@
+package com.errera.pro;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		if (args != null) System.out.println("avec args ---------$$$$$$$$$--------------- " + args[0]);
+		else System.out.println("sans args-----$$$$$$$$$$$$$$$---------------");
+
+	}
+
+}
